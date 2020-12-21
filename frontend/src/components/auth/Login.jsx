ac45a24aaf4b728a06a2765a74728be22ce56a8d
michaelmoreno <michaelmoreno.dev@gmail.com>
2020-12-20 22:41:17 -0600

    Login form working.

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { useHistory } from 'react-router-dom';
+import Axios from 'axios';
+import UserContext from '../../context/UserContext';
 
 export default function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const { setUserData } = useContext(UserContext);
+  const history = useHistory();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const loginUser = { email, password };
+    const loginRes = await Axios.post('http://localhost:3000/users/login', loginUser)
+    setUserData({
+      token: loginRes.data.token,
+      user: loginRes.data.user
+    });
+    localStorage.setItem('auth-token', loginRes.data.token) //can I just do token instead?
+    history.push('/');
+  }
+
+  
   return (
-    <div>
-      Login
+    <div id="login">
+      <h2>Login</h2>
+      <form onSubmit={submit}>
+        <label htmlFor="login-email">Email</label>
+        <input type="email" id="register-email" onChange={e => setEmail(e.target.value)} />
+
+        <label htmlFor="login-password">Password</label>
+        <input type="text" id="login-password" onChange={e => setPassword(e.target.value)} />
+        <input type="submit" value="login" />
+      </form>
     </div>
   )
 }
